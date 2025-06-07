942a3129cfd6d30306b2c97c40dab8343b57a013
jariuock <garanin.019@gmail.com>
2025-06-07 17:19:13 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,25 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <memory>
+#include <functional>
 
+class Node {
+public:
+    char val;
+    std::vector<std::shared_ptr<Node>> children;
+
+    Node(char val) : val(val) {}
+};
+
+class PMTree {
+public:
+    std::shared_ptr<Node> root;
+    PMTree(const std::vector<char>& elements);
+};
+
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
 #endif  // INCLUDE_TREE_H_
