9b99e12b443baa79e928236030dc7045eeff201c
jariuock <garanin.019@gmail.com>
2025-06-07 17:29:57 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,44 @@
 #include <numeric>
 #include <vector>
 #include "tree.h"
-using namespace std;
 
 int main() {
-    cout << "n\tgetAllPerms(ms)\tgetPerm1(ms)\tgetPerm2(ms)" << endl;
+  std::cout << "n\tgetAllPerms(ms)\tgetPerm1(ms)\tgetPerm2(ms)" << std::endl;
 
- for (int n = 2; n <= 10; ++n) {
-     vector<char> input(n);
-     iota(input.begin(), input.end(), '1');
+  // Инициализация генератора случайных чисел
+  std::mt19937 rng(static_cast<unsigned int>(
+      std::chrono::steady_clock::now().time_since_epoch().count()));
 
-     PMTree tree(input);
+  for (int n = 2; n <= 10; ++n) {
+    std::vector<char> input(n);
+    std::iota(input.begin(), input.end(), '1');
 
-     int perm_count = 1;
-     for (int i = 2; i <= n; ++i)
-         perm_count *= i;
+    PMTree tree(input);
 
-     int num = rand() % perm_count + 1;
+    int perm_count = 1;
+    for (int i = 2; i <= n; ++i) {
+      perm_count *= i;
+    }
 
-     auto t1 = chrono::high_resolution_clock::now();
-     auto all = getAllPerms(tree);
-     auto t2 = chrono::high_resolution_clock::now();
-     double t_all = chrono::duration<double, std::milli>(t2 - t1).count();
+    std::uniform_int_distribution<int> dist(1, perm_count);
+    int num = dist(rng);
 
-     t1 = chrono::high_resolution_clock::now();
-     auto p1 = getPerm1(tree, num);
-     t2 = chrono::high_resolution_clock::now();
-     double t_p1 = chrono::duration<double, std::milli>(t2 - t1).count();
+    auto t1 = std::chrono::high_resolution_clock::now();
+    auto all = getAllPerms(tree);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    double t_all = std::chrono::duration<double, std::milli>(t2 - t1).count();
 
-     t1 = chrono::high_resolution_clock::now();
-     auto p2 = getPerm2(tree, num);
-     t2 = chrono::high_resolution_clock::now();
-     double t_p2 = chrono::duration<double, std::milli>(t2 - t1).count();
+    t1 = std::chrono::high_resolution_clock::now();
+    auto p1 = getPerm1(tree, num);
+    t2 = std::chrono::high_resolution_clock::now();
+    double t_p1 = std::chrono::duration<double, std::milli>(t2 - t1).count();
 
-     cout << n << "\t" << t_all << "\t\t" << t_p1 << "\t\t" << t_p2 << endl;
- }
+    t1 = std::chrono::high_resolution_clock::now();
+    auto p2 = getPerm2(tree, num);
+    t2 = std::chrono::high_resolution_clock::now();
+    double t_p2 = std::chrono::duration<double, std::milli>(t2 - t1).count();
+
+    std::cout << n << "\t" << t_all << "\t\t" << t_p1 << "\t\t" << t_p2 << std::endl;
+  }
   return 0;
 }
